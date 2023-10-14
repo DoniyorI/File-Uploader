0a6e35ad9a743b465a13f276617bc4840a8e951c
Doniyor <doniyori@buffalo.edu>
2023-10-14 16:03:51 -0400

    Added CSS and Small Adjustments

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ ALLOWED_EXTENSIONS = {'pdf'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/tutorial', methods=['GET', 'POST'])
 def upload_file():
     files = []
     if os.path.exists('pdf_descriptions.csv'):
